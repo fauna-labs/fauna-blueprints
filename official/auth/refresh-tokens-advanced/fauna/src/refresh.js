bcae7d2ea543917569fa8a37fe27480e61400197
Brecht De Rooms <databrecht@gmail.com>
2021-03-24 16:11:21 +0100

    Remove unused line

--- official/auth/refresh-tokens-advanced/fauna/src/refresh.js
+++ official/auth/refresh-tokens-advanced/fauna/src/refresh.js
@@ -4,8 +4,6 @@ import { RotateAccessAndRefreshToken, VerifyRefreshToken } from './tokens'
 const q = faunadb.query
 const { CurrentIdentity, Get } = q
 
-export const GRACE_PERIOD_SECONDS = 20// 20 seconds
-
 export function RefreshToken (gracePeriodSeconds, accessTtlSeconds, refreshLifetimeSeconds, refreshReclaimtimeSeconds) {
   return VerifyRefreshToken(
     {
