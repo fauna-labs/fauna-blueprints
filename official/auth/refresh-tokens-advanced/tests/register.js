5e5fc5e2366b744e50bae9c0f6dbdf35c7503f71
Brecht De Rooms <databrecht@gmail.com>
2021-03-30 21:18:39 +0200

    add public role

--- official/auth/refresh-tokens-advanced/tests/register.js
+++ official/auth/refresh-tokens-advanced/tests/register.js
@@ -2,29 +2,31 @@ import test from 'ava'
 import path from 'path'
 import * as fauna from 'faunadb'
 import * as schemaMigrate from 'fauna-schema-migrate'
-import { destroyTestDatabase, setupTestDatabase, populateDatabaseSchemaFromFiles } from '../../../../util/helpers/setup-db'
+import { destroyTestDatabase, setupTestDatabase, populateDatabaseSchemaFromFiles, getClient } from '../../../../util/helpers/setup-db'
 const q = fauna.query
-const { Call, Paginate, Documents, Collection, Lambda, Get } = q
+const { Call, Paginate, Documents, Collection, Lambda, Get, CreateKey, Role } = q
 
-const testName = path.basename(__filename)
-test.before(async (t) => {
+let index = 0
+test.beforeEach(async (t) => {
   // Set up the child database and retrieve both a fauna Client
   // to query the database as parent database.
-  t.context.databaseClients = await setupTestDatabase(fauna, testName)
-})
-
-test.after.always(async (t) => {
-  // Destroy the child database to clean up (using the parentClient)
-  await destroyTestDatabase(q, testName, t.context.databaseClients.parentClient)
-})
-
-test(testName + ': verify account was created', async t => {
+  t.context.testName = path.basename(__filename) + (index = ++index)
+  t.context.databaseClients = await setupTestDatabase(fauna, t.context.testName)
   const client = t.context.databaseClients.childClient
-  // Populate the schema on the child database.
   await populateDatabaseSchemaFromFiles(schemaMigrate, q, client, [
     'fauna/resources/collections/accounts.fql',
-    'fauna/resources/functions/register.fql'
+    'fauna/resources/functions/register.fql',
+    'fauna/resources/roles/public.fql'
   ])
+})
+
+test.afterEach.always(async (t) => {
+  // Destroy the child database to clean up (using the parentClient)
+  await destroyTestDatabase(fauna.query, t.context.testName, t.context.databaseClients.parentClient)
+})
+
+test(path.basename(__filename) + ': verify account was created', async t => {
+  const client = t.context.databaseClients.childClient
   // We now have a register function which we can callbundleRenderer.renderToStream
   await client.query(Call('register', 'brecht@brechtsdomain.be', 'verysecure'))
   const accounts = await client.query(q.Map(
@@ -37,4 +39,12 @@ test(testName + ': verify account was created', async t => {
   t.is(accounts.data[0].data.email, 'brecht@brechtsdomain.be')
   // Passwords are never returned
   t.is(accounts.data[0].data.password, undefined)
+})
+
+test(path.basename(__filename) + ': you can register with a key that uses the public role', async t => {
+  const client = t.context.databaseClients.childClient
+  const key = await client.query(CreateKey({ role: Role('public') }))
+  const publicClient = getClient(fauna, key.secret)
+  const res = await publicClient.query(Call('register', 'brecht@brechtsdomain.be', 'verysecure'))
+  t.truthy(res)
 })
--- official/auth/register-login-logout/tests/register.js
+++ official/auth/register-login-logout/tests/register.js
@@ -1,31 +1,33 @@
 import test from 'ava'
-import { destroyTestDatabase, setupTestDatabase, populateDatabaseSchemaFromFiles } from '../../../../util/helpers/setup-db'
+import path from 'path'
+import { destroyTestDatabase, setupTestDatabase, populateDatabaseSchemaFromFiles, getClient } from '../../../../util/helpers/setup-db'
 import * as fauna from 'faunadb'
 import * as schemaMigrate from 'fauna-schema-migrate'
 const q = fauna.query
-const { Call, Paginate, Documents, Collection, Lambda, Get } = q
+const { Call, Paginate, Documents, Collection, Lambda, Get, CreateKey, Role } = q
 
-const testName = 'register'
-let databaseClients = { parentClient: null, childClient: null }
-test.before(async (t) => {
+let index = 0
+test.beforeEach(async (t) => {
   // Set up the child database and retrieve both a fauna Client
   // to query the database as parent database.
-  databaseClients = await setupTestDatabase(fauna, testName)
-})
-
-test.after.always(async (t) => {
-  // Destroy the child database to clean up (using the parentClient)
-  await destroyTestDatabase(q, testName, databaseClients.parentClient)
-})
-
-test(testName + ': verify account was created', async t => {
-  t.plan(2)
-  const client = databaseClients.childClient
-  // Populate the schema on the child database.
+  t.context.testName = path.basename(__filename) + (index = ++index)
+  t.context.databaseClients = await setupTestDatabase(fauna, t.context.testName)
+  const client = t.context.databaseClients.childClient
   await populateDatabaseSchemaFromFiles(schemaMigrate, q, client, [
     'fauna/resources/collections/accounts.fql',
-    'fauna/resources/functions/register.fql'
+    'fauna/resources/functions/register.fql',
+    'fauna/resources/roles/public.fql'
   ])
+})
+
+test.afterEach.always(async (t) => {
+  // Destroy the child database to clean up (using the parentClient)
+  await destroyTestDatabase(fauna.query, t.context.testName, t.context.databaseClients.parentClient)
+})
+
+test(path.basename(__filename) + ': verify account was created', async t => {
+  t.plan(2)
+  const client = t.context.databaseClients.childClient
   // We now have a register function which we can callbundleRenderer.renderToStream
   await client.query(Call('register', 'brecht@brechtsdomain.be', 'verysecure'))
   const accounts = await client.query(q.Map(
@@ -38,4 +40,12 @@ test(testName + ': verify account was created', async t => {
   t.is(accounts.data[0].data.email, 'brecht@brechtsdomain.be')
   // Passwords are never returned
   t.is(accounts.data[0].data.password, undefined)
+})
+
+test(path.basename(__filename) + ': you can register with a key that uses the public role', async t => {
+  const client = t.context.databaseClients.childClient
+  const key = await client.query(CreateKey({ role: Role('public') }))
+  const publicClient = getClient(fauna, key.secret)
+  const res = await publicClient.query(Call('register', 'brecht@brechtsdomain.be', 'verysecure'))
+  t.truthy(res)
 })
